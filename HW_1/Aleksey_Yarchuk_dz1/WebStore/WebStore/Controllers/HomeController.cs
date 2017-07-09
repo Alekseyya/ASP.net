764ed496a38c4c5f23ffde49b240e96a4db17e49
Fokiruna <alekseyyarchuk@gmail.com>
2017-07-09 21:31:22 +0300

    Сделаны первые два задания

---
+++ HW_1/Aleksey_Yarchuk_dz1/WebStore/WebStore/Controllers/HomeController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public List<Employee> employees = new List<Employee>
+            {
+                new Employee
+                {
+                    id=1,
+                    FirstName = "Алексей",
+                    LastName = "Генычув",
+                    Partonymic = "Григорьев",
+                    Age = 23,
+                    Position = "Генерал иракской разведки",
+                    DateofBirth = new DateTime(2000, 3, 15)
+                    
+                },
+                new Employee
+                {
+                    id = 2,
+                    FirstName = "Афанасий",
+                    LastName = "Батькович",
+                    Partonymic = "Алексеев",
+                    Age = 28,
+                    Position = "Старший прапорщик",
+                    DateofBirth = new DateTime(1998, 4, 15)
+                },
+                new Employee
+                {
+                    id = 3,
+                    FirstName = "Мария",
+                    LastName = "Конь",
+                    Partonymic = "Беловна",
+                    Age = 27,
+                    Position = "Повар",
+                    DateofBirth = new DateTime(1665, 9, 18)
+                }
+            };
+        // GET: Home
+        public ActionResult Index()
+        {            
+            return View(employees);
+        }
+        
+        public ActionResult Result(int id)
+        {
+            var employee = FindEmployee(id);
+            if (employee != null)
+            {
+                return View(employee);
+            }
+            return RedirectToAction("Index");          
+        }
+        public Employee FindEmployee(int id)
+        {
+            foreach (var employee in employees)
+            {
+                if (employee.id == id)
+                {
+                    return employee;
+                }
+            }
+            return null;
+        }
+        [HttpGet]
+        public ActionResult EditEmployee(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            var employee = FindEmployee((int)id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(employee);
+        }
+
+        [HttpPost]
+        public ActionResult EditEmployee(Employee emp)
+        {
+            var foundPerson = FindEmployee(emp.id);
+
+            foundPerson.FirstName = emp.FirstName;
+            foundPerson.LastName = emp.LastName;
+            foundPerson.Age = emp.Age;
+            return RedirectToAction("Index");
+        }
+
+
+
+    }
+}
---
+++ HW_1/Aleksey_Yarchuk_dz1/WebStore/WebStore/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebStore.Models
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Partonymic { get; set; }
+        public int Age { get; set; }
+        public string Position { get; set; }
+        
+        public DateTime DateofBirth { get; set; }
+
+    }
+}
