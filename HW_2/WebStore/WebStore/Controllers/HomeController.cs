0af81f2ff45b7200fdf9f288fd962db925628c12
Fokiruna <alekseyyarchuk@gmail.com>
2017-07-12 22:59:56 +0300

    Ошибка при валидации

---
+++ HW_2/WebStore/WebStore/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        // GET: Home
+        public ActionResult Index()
+        {                      
+            return View(db.employees);
+        }
+        
+        public ActionResult Result(int id)
+        {
+            var employee = db.GetById(id);
+            if (employee != null)
+            {
+                return View(employee);
+            }
+            return RedirectToAction("Index");          
+        }
+        
+        [HttpGet]
+        public ActionResult EditEmployee(int? id)
+        {
+            if (id == null)
+            {
+                return View("EditEmployee");
+            }
+            var employee = db.GetById((int)id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(employee);
+        }
+
+        [HttpPost]
+        public ActionResult EditEmployee(Employee emp)
+        {
+            if (ModelState.IsValid)
+            {
+                var foundPerson = db.GetById(emp.id);
+                if (foundPerson != null)
+                    db.Edit(emp);
+                else
+                    db.Add(emp);
+               return RedirectToAction("Index");
+            }
+
+            return View(emp);
+        }
+
+        
+        public ActionResult Delete(int id)
+        {
+            db.Delete(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ HW_2/WebStore/WebStore/Models/Employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebStore.Models
+{
+    public class Employee
+    {
+        public int id { get; set; }
+
+        [Required(AllowEmptyStrings =false, ErrorMessage ="Неправильно введено имя")]
+        [Display(Name ="Имя")]
+        [StringLength(20, MinimumLength =3, ErrorMessage ="Имя должно состоять от 3 до 20 символов")]
+        [RegularExpression(@"[А-Я][а-я]{3,20}", ErrorMessage = "Несоответствует паттерну")]        
+        public string FirstName { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Неправильно введена фамилия")]
+        [Display(Name = "Фамилия")]
+        [StringLength(25, MinimumLength = 5, ErrorMessage = "Фамилия должно состоять от 5 до 25 символов")]        
+        public string LastName { get; set; }
+
+        [StringLength(25, MinimumLength = 5, ErrorMessage = "Отчество должно состоять от 5 до 25 символов")]
+        [Display(Name = "Отчество")]
+        public string Partonymic { get; set; }
+        
+        [Range(18, 65)]
+        [Display(Name = "Возраст")]
+        public int Age { get; set; }
+
+        [StringLength(50,MinimumLength = 3, ErrorMessage = "Возраст должно состоять от 1 до 2 символов")]
+        [Display(Name = "Должность")]
+        public string Position { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Дата рождения является обязательной")]
+        //[DataType(DataType.Date)]
+        //[DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "dd/MM/yyyy", ConvertEmptyStringToNull = true)]
+        [Display(Name="Дата рождения")]
+        public DateTime DateofBirth { get; set; }
+
+    }
+}
