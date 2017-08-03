3ed9b619be6c5578cf8f79d79e4d1f4a3902efc0
Fokiruna <alekseyyarchuk@gmail.com>
2017-08-03 18:21:03 +0300

    Обновлен OrderController

--- HW_8/WebStore.WebUi/WebStore.WebUi/Controllers/OrderController.cs
+++ HW_8/WebStore.WebUi/WebStore.WebUi/Controllers/OrderController.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebStore.WebUi.Models.Order;
+using WebStore.WebUi.OrderService;
 
 namespace WebStore.WebUi.Controllers
 {
@@ -12,16 +15,53 @@ namespace WebStore.WebUi.Controllers
         // GET: Order
         public ActionResult Index() //Информация о заказе
         {
-            //using (var client = new OrderService) // получаем данные заказа
-            //{
-
-            //}
-            return View(); //new ienumerableUserorderViewModel
+            List<UserOrderViewModel> orders = new List<UserOrderViewModel>();
+            using (var client = new OrderServiceClient()) // получаем данные заказа
+            {
+                foreach (var order in client.GetOrders().Where(u=>u.User == System.Web.HttpContext.Current.User.Identity.Name))
+                {
+                    orders.Add(new UserOrderViewModel
+                    {
+                        Id = order.Id,
+                        UserName = order.User,
+                        OrderPrice = order.OrderPrice,
+                        OrderDate = order.OrderDate
+                     });
+                }
+                
+            }
+            return View(orders.AsEnumerable()); 
         }
-        public ActionResult OrderHistoryDetails(int id, DateTime orderDate)
+        public ActionResult OrderHistoryDetails(DateTime? orderDate) //id - номер заказа
         {
+            
+            UserOrderDetailsViewModel orderDetails = new UserOrderDetailsViewModel();
+            List<UserOrderDetailsItem> orderDetailsItems = new List<UserOrderDetailsItem>();
+            using (var client = new OrderServiceClient()) // получаем данные заказа
+            {
+                //надо найти сам заказа
+                var order = client.GetOrders().FirstOrDefault(o => o.OrderDate == orderDate);
+                orderDetails.Id = order.Id;
+                orderDetails.UserName = System.Web.HttpContext.Current.User.Identity.Name;
+                orderDetails.OrderPrice = order.OrderPrice;
+                orderDetails.Details = new Func<IEnumerable<UserOrderDetailsItem>>(() =>
+                    {
+                        foreach (var orderItem in client.GetOrderDetails().Where(o => o.OrderId == order.Id))
+                        {
+                            orderDetailsItems.Add(new UserOrderDetailsItem
+                            {
+                                Id = orderItem.Id,
+                                ProductName = orderItem.Product,
+                                Quantity = orderItem.Quantity,
+                                TotalPrice = orderItem.TotalPrice
+                            });
+                        }
+                        return orderDetailsItems.AsEnumerable();
+                    })();
+                
 
-            return View(); //UserOrderDetailsViewModel
+            }            
+            return View(orderDetails); //UserOrderDetailsViewModel
         }
     }
 }
