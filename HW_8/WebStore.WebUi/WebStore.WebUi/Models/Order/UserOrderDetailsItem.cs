68b52a1ca73d8fe96395651db3f8eaefcc514e5c
Fokiruna <alekseyyarchuk@gmail.com>
2017-08-04 04:16:35 +0300

    Указана разметка для полей модели

--- HW_8/WebStore.WebUi/WebStore.WebUi/Models/Order/UserOrderDetailsItem.cs
+++ HW_8/WebStore.WebUi/WebStore.WebUi/Models/Order/UserOrderDetailsItem.cs
@@ -1,10 +1,27 @@
-﻿namespace WebStore.WebUi.Models.Order
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebStore.WebUi.Models.Order
 {
     public class UserOrderDetailsItem
     {
+        [Display(Name="Id")]
         public int Id { get; set; }
+
+        [Display(Name = "Наименование")]
         public string ProductName { get; set; }
+
+        [Display(Name = "Количество")]
         public int Quantity { get; set; }
+
+        [Display(Name ="Цена за штуку")]
         public decimal TotalPrice { get; set; }
+
+        [Display(Name ="Общая стоимость")]
+        public decimal Price {
+            get
+            {
+                return Quantity * TotalPrice;
+            }
+        }
     }
 }
