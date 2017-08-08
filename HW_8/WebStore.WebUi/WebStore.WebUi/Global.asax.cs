f0ebb1cdc693963d6a8ebf9dfbe3bed7499ea21d
Fokiruna <alekseyyarchuk@gmail.com>
2017-08-08 19:49:51 +0300

    Добавлена ссылка на конфиг к мапперу

--- HW_8/WebStore.WebUi/WebStore.WebUi/Global.asax.cs
+++ HW_8/WebStore.WebUi/WebStore.WebUi/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using WebStore.WebUi.App_Start;
 
 namespace WebStore.WebUi
 {
@@ -16,6 +17,8 @@ namespace WebStore.WebUi
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            AutoMapperWebConfiguration.Configure();
         }
     }
 }
