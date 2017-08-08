47a3d0bcdef53755fe2cd6773925032a829592e9
Fokiruna <alekseyyarchuk@gmail.com>
2017-08-08 19:49:10 +0300

    Добавлен конфиг для маппера

---
+++ HW_8/WebStore.WebUi/WebStore.WebUi/App_Start/AutoMapperWebConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebStore.WebUi.Models;
+
+namespace WebStore.WebUi.App_Start
+{
+    public static class AutoMapperWebConfiguration
+    {
+        public static void Configure()
+        {
+            Mapper.Initialize(cfg =>
+            {
+                cfg.AddProfile(new ProductProfile());                
+            });
+        }
+    }
+    public class ProductProfile : Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<service.ProductDataContract, IndexProductViewModel>();
+        }
+        
+    }
+}
