2d1cf41dd11362887a6deeca20d2bfc9ba9c8ec8
Fokiruna <alekseyyarchuk@gmail.com>
2017-08-16 15:22:01 +0300

    Добавлен класс Марка

---
+++ HW_8/WebStore.WebUi/WebStore.Domain/Entities/Marka.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebStore.Domain.Entities
+{
+   public class Marka
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Main { get; set; }
+    }
+}
