b45d0d7c7996ec4454a0bfad3bf04f9f55b0488d
Fokiruna <alekseyyarchuk@gmail.com>
2017-08-16 18:11:39 +0300

    Перемеланная марка

---
+++ HW_8/WebStore.WebUi/WebStore.Domain/Entities/Mark.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebStore.Domain.Entities
+{
+   public class Marka
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Main { get; set; }
+    }
+    public class MarkaConfiguration : EntityTypeConfiguration<Marka>
+    {
+        public MarkaConfiguration()
+        {
+            HasKey(x => x.Id);
+
+            Property(x => x.Id)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity)
+                .IsRequired();
+
+            Property(x => x.Name)
+                .HasMaxLength(25)
+                .IsRequired();
+        }
+    }
+}
